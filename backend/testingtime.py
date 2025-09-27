4102e904e02a69c3d2dd60ef43c97d3b8033e902
ryanping <ryantram2005@gmail.com>
2025-09-27 17:50:48 -0400

    changed icons

---
+++ backend/testingtime.py
@@ -0,0 +1,22 @@
+# using SendGrid's Python Library
+# example on how it would work for messaging
+# https://github.com/sendgrid/sendgrid-python
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+message = Mail(
+    from_email=os.environ.get('SENDER_EMAIL'),
+    to_emails='ryantram2005@gmail.com',
+    subject='Sending with Twilio SendGrid is Fun',
+    html_content='<strong>and easy to do anywhere, even with Python</strong>')
+try:
+    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+    # sg.set_sendgrid_data_residency("eu")
+    # uncomment the above line if you are sending mail using a regional EU subuser
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+except Exception as e:
+    print(e)
--- frontend/StudBud/app/(tabs)/_layout.tsx
+++ frontend/StudBud/app/(tabs)/_layout.tsx
@@ -2,26 +2,28 @@
 
 import React from 'react';
 import { Tabs } from 'expo-router';
+
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: '#6200ee', // Your theme's primary color
+        tabBarActiveTintColor: '#aeb3ffff', // Your theme's primary color
       }}>
-      <Tabs.Screen
-        name="index" // This links to index.tsx
-        options={{
-          title: 'Feed',
-          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="home" color={color} size={28} />,
-        }}
-      />
       <Tabs.Screen
         name="explore" // This links to explore.tsx
         options={{
           title: 'Explore',
           tabBarIcon: ({ color }) => <MaterialCommunityIcons name="magnify" color={color} size={28} />,
+        }}
+      />
+      <Tabs.Screen
+        name="index" // This links to index.tsx
+        options={{
+          title: 'Post',
+          tabBarIcon: ({ color }) => <FontAwesome name="plus" color={color} size={28} />,
         }}
       />
       {/* You can add more tabs here, like for a profile screen */}
